f5ae5251d252fd0315ab1dda6c857a19f69c54c3
zacharyofsweden <zacharyofsweden@gmail.com>
2025-05-18 19:53:33 +0200

    Uppdated app js to work with calander

--- frontend/App.js
+++ frontend/App.js
@@ -3,22 +3,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
+import { AuthProvider } from './contexts/AuthContext';
 import { TaskProvider } from './contexts/TaskContext';
 import { ZooProvider } from './contexts/ZooContext';
 
-import AppRouter from './navigation/AppRouter'; 
+import AppRouter from './navigation/AppRouter';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <TaskProvider>
-        <ZooProvider>
-          <NavigationContainer>
-            <AppRouter />
-            <StatusBar style="auto" />
-          </NavigationContainer>
-        </ZooProvider>
-      </TaskProvider>
+      <AuthProvider>
+        <TaskProvider>
+          <ZooProvider>
+            <NavigationContainer>
+              <AppRouter />
+              <StatusBar style="auto" />
+            </NavigationContainer>
+          </ZooProvider>
+        </TaskProvider>
+      </AuthProvider>
     </SafeAreaProvider>
   );
 }
